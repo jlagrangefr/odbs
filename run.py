a0d422765a8bfab5e1b08a8aa31f0c3de03b7e7c
Jonatan LAGRANGE <jonatan@larchiviste.fr>
2023-10-18 12:53:53 +0200

    Adding venv detection

--- run.py
+++ run.py
@@ -1,5 +1,11 @@
 # Calls the odbs.py file containing the ODBS class
 from odbs import Odbs
+import sys
+
+if sys.prefix == sys.base_prefix:
+    print("Please run this script from the python venv!")
+    print("Scripts/python run.py")
+    exit()
 
 # Load config file
 import json
